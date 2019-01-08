c9aceef2793c0d1b8f9de415dac98c565acfb1ff
joseenriqe97 <joseenriqe92@gmail.com>
2019-01-08 15:24:13 -0400

    Add methods and attributes.

--- app/src/main/java/com/example/bonilla/seccion_02/GridActivity.java
+++ app/src/main/java/com/example/bonilla/seccion_02/GridActivity.java
@@ -2,12 +2,69 @@ package com.example.bonilla.seccion_02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GridActivity extends AppCompatActivity {
+
+    private List<String> names;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid);
+
+        listView = findViewById(R.id.listView);
+
+        //Datos a mostrar
+        names = new ArrayList<String>();
+        names.add("Jose Enrique");
+        names.add("Pedro");
+        names.add("Juan");
+        names.add("Felipe");
+        names.add("Jose Enrique");
+        names.add("Pedro");
+        names.add("Juan");
+        names.add("Felipe");
+        names.add("Jose Enrique");
+        names.add("Pedro");
+        names.add("Juan");
+        names.add("Felipe");
+        names.add("Jose Enrique");
+        names.add("Pedro");
+        names.add("Juan");
+        names.add("Felipe");
+        names.add("Jose Enrique");
+        names.add("Pedro");
+        names.add("Juan");
+        names.add("Felipe");
+        names.add("Jose Enrique");
+        names.add("Pedro");
+        names.add("Juan");
+        names.add("Felipe");
+
+
+
+        //Adaptador, la forma visual en que mostraremos los datos.
+        // ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names); //Se le pasa el layaout que queremo que renderice en la vista.
+
+        //Enlazamos el adaptador con nuestro listView.
+        // listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Your name is " + names.get(position   ), Toast.LENGTH_LONG).show();
+            }
+        });
+        //Enlazamos con nuestro adaptador.
+        MyAdapter myAdapter = new MyAdapter(this, R.layout.list_item, names);
+        listView.setAdapter(myAdapter);
     }
 }
